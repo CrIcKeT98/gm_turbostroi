af012e8b95150d6d5c73dc60c95d603873213557
CrIcKeT <lezgin1998@yandex.ru>
2023-01-06 17:03:59 +0300

    Updated premake

--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,8 @@
 PROJECT_GENERATOR_VERSION = 3
 
+local workspace_name = "turbostroi"
+local project_serverside = true
+
 newoption({
 	trigger = "gmcommon",
 	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
@@ -8,11 +11,40 @@ newoption({
 
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
 	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
-include(gmcommon)
+include(gmcommon .. "/generator.v2.lua")
 
-CreateWorkspace({name = "gm_turbostroi_gays", abi_compatible = false, path = "projects/" .. os.target() .. "/" .. _ACTION})
-	CreateProject({serverside = true, source_path = "source", manual_files = false})
-		IncludeSDKCommon()
-		IncludeSDKTier0()
-		IncludeSDKTier1()
-		
+sysincludedirs { "source/include", 
+				"external/LuaJIT/src", 
+				"external/boost/libs"
+				}
+				
+--Fix include order
+removesysincludedirs 	{ "external/garrysmod_common/include", "external/garrysmod_common/helpers/include" }
+sysincludedirs 			{ "external/garrysmod_common/include", "external/garrysmod_common/helpers/include" }
+
+
+CreateWorkspace({name = "gm_turbostroi", abi_compatible = false, path = "projects/" .. os.target() .. "/" .. _ACTION})
+
+CreateProject({serverside = true, source_path = "source", manual_files = false})
+
+filter({"system:windows", "architecture:x86"})
+	libdirs("external/LuaJIT/x86")
+	
+filter({"system:windows", "architecture:x86_64"})
+	libdirs("external/LuaJIT/x64")
+
+filter("system:windows or macosx")
+	links("lua51", "luajit")
+
+filter({"system:linux", "architecture:x86"})
+	libdirs("external/LuaJIT/linux32")
+	links("lua51", "luajit")
+
+filter({"system:linux", "architecture:x86_64"})
+	links("lua51", "luajit")
+
+filter({})
+
+IncludeSDKCommon()
+IncludeSDKTier0()
+IncludeSDKTier1()
